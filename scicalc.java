d94786c99b42f69343867f682eea0ce4125f0d37
hadil alalawi <had33l-@outlook.com>
2025-07-20 14:36:53 +0400

    add switch cases

--- scicalc.java
+++ scicalc.java
@@ -13,12 +13,31 @@ public class scicalc {
     while (true){
         displayMenu();
         System.out.println("pick your choices :) ");
+        switch (choice) {
+            case1:
+
+            //
+
+            break;
+
+            case2:
+
+            //
+
+            break;
+
+            case3:
+
+            //
 
 
+            System.out.println("Goodbye the sci calc was here :> !");
+            wewe.close();
+            return; // exits main
+            default:
+                System.out.println("Invalid choice. Try again.");
 
-
-
-
+        }
     }
 
 
