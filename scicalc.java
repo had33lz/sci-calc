08a205bd98753a3d66177fb4100989263bff11ef
hadil alalawi <had33l-@outlook.com>
2025-07-20 14:31:44 +0400

    while loop

--- scicalc.java
+++ scicalc.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+
+import java.util.Scanner;
+import java.lang.*;
+
 
 public class scicalc {
+
+    public static void main(String[] args) {
+
+    Scanner wewe = new Scanner(System.in);
+
+
+    while (true){
+        displayMenu();
+        System.out.println("pick your choices :) ");
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    }
+
+
+
 }
